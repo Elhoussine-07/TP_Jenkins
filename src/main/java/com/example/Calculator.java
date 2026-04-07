767c3c7c5b82148ee443e81c8deba722e81b14c9
Elhoussine-07 <ellahoussine1@gmail.com>
2026-04-07 11:57:15 +0100

    Attribute full name of each function in comments

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -2,22 +2,22 @@ package com.example;
 
 public class Calculator {
 
-
+    //addition
     public int add(int a, int b) {
         return a + b;
     }
 
-
+    //substraction
     public int subtract(int a, int b) {
         return a - b;
     }
 
-
+    //multiplication
     public int multiply(int a, int b) {
         return a * b;
     }
 
-
+   //division
     public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division par zéro impossible");

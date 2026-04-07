1078086fe66a4cec83cc581228b2941d5c1b2890
Elhoussine-07 <ellahoussine1@gmail.com>
2026-04-07 12:01:02 +0100

    duplicate comments

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -3,20 +3,24 @@ package com.example;
 public class Calculator {
 
     //addition
+    //addition
     public int add(int a, int b) {
         return a + b;
     }
 
+    //substraction
     //substraction
     public int subtract(int a, int b) {
         return a - b;
     }
 
     //multiplication
+    //multiplication
     public int multiply(int a, int b) {
         return a * b;
     }
 
+   //division
    //division
     public int divide(int a, int b) {
         if (b == 0) {
